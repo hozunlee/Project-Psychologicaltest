834e05963da81fa5febd551c7ace4c8493f472eb
hojunlee <ho2yahh@gmail.com>
2021-11-16 21:49:50 +0900

    진짜돌아가는home

--- src/App.js
+++ src/App.js
@@ -1,43 +1,20 @@
-<<<<<<< HEAD
 import React from 'react';
 import {useState} from 'react';
 import './App.css';
 import reactDom from 'react-dom';
-
 import Home from './components/Home';
 import QuestionPage from './components/QuestionPage';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> 35c9485b90bcf2ee2e739fb0efd9b42e47c0bdf6
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Router>
         <Route path="/" exact component={Home} />
         <Route path="/QuestionPage" component={QuestionPage} />
       </Router>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 35c9485b90bcf2ee2e739fb0efd9b42e47c0bdf6
     </div>
   );
 }
